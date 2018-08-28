fd11c376e5cc945995ded62c0895f70fbd88cd88
Rajat Upadhyay <ratziiee@Rajats-MacBook-Air.local>
2018-08-28 20:05:45 +0530

    second commit

---
+++ app/src/main/java/org/ratziiee/qlap/Registration/Main_application/Driver/Fragments/my_trip_driver/trip_driver_1.java
@@ -0,0 +1,35 @@
+package org.ratziiee.qlap.Registration.Main_application.Driver.Fragments.my_trip_driver;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.ratziiee.qlap.R;
+import org.ratziiee.qlap.Registration.Utils.utils;
+
+public class trip_driver_1 extends AppCompatActivity {
+
+    Button btn_start_trip;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_driver_1);
+
+        TextView toolbar_title=findViewById(R.id.toolbar_title);
+        toolbar_title.setText("My RIDE");
+        utils utils=new utils();
+        utils.change_status_bar_color(this,this);
+
+        btn_start_trip=findViewById(R.id.btn_satrt);
+        btn_start_trip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(trip_driver_1.this,trip_driver_2.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/org/ratziiee/qlap/Registration/Main_application/Driver/Fragments/my_trip_driver/trip_driver_2.java
@@ -0,0 +1,15 @@
+package org.ratziiee.qlap.Registration.Main_application.Driver.Fragments.my_trip_driver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.ratziiee.qlap.R;
+
+public class trip_driver_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_driver_2);
+    }
+}
